16f2f006b8df90d5ee8df77178fa986256758ac3
Tabithadejong <tabithadejong654@gmail.com>
2023-03-10 15:58:59 +0000

    Even further

--- codestar/settings.py
+++ codestar/settings.py
@@ -40,12 +40,24 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', 
+    'django.contrib.sites',
+    'allauth',
+    'allauth.socialaccount',
+    'allauth.account',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote', 
+    'django_summernote',
+    'crispy_forms',
     'blog',
 ]
+
+SITE_ID= 1 
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
